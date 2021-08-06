5e1ac7042450ce2ffeaec3d8071f5f9c9cbdd8b0
Psilon <pzixel@gmail.com>
2021-08-06 19:24:36 +0300

    Format seems to be working

--- TestApp.UnitTest/ParserTests.cs
+++ TestApp.UnitTest/ParserTests.cs
@@ -138,6 +138,11 @@ namespace TestApp.UnitTest
             {
                 Assert.Equal((int) dayOfWeek, ParserHelper.DayOfWeekParser.ParseOrThrow(dayOfWeek.ToString("D")).Single().Begin);
             }
+            Assert.Equal(new []
+            {
+                ScheduleFormatEntry.SinglePoint(1),
+                new ScheduleFormatEntry(2, 4, null)
+            }, ParserHelper.DayOfWeekParser.ParseOrThrow("1,2-4"));
             Assert.Throws<ParseException>(() => ParserHelper.DayOfWeekParser.ParseOrThrow("7"));
         }
 
@@ -190,44 +195,6 @@ namespace TestApp.UnitTest
             Assert.Throws<ParseException>(() => ParserHelper.TimeParser.ParseOrThrow("00:00:00.1000"));
         }
 
-        /// <summary>
-        /// Создает экземпляр из строки с представлением расписания.
-        /// </summary>
-        /// <param name="scheduleString">Строка расписания.
-        /// Формат строки:
-        ///     yyyy.MM.dd w HH:mm:ss.fff
-        ///     yyyy.MM.dd HH:mm:ss.fff
-        ///     HH:mm:ss.fff
-        ///     yyyy.MM.dd w HH:mm:ss
-        ///     yyyy.MM.dd HH:mm:ss
-        ///     HH:mm:ss
-        /// Где yyyy - год (2000-2100)
-        ///     MM - месяц (1-12)
-        ///     dd - число месяца (1-31 или 32). 32 означает последнее число месяца
-        ///     w - день недели (0-6). 0 - воскресенье, 6 - суббота
-        ///     HH - часы (0-23)
-        ///     mm - минуты (0-59)
-        ///     ss - секунды (0-59)
-        ///     fff - миллисекунды (0-999). Если не указаны, то 0
-        /// Каждую часть даты/времени можно задавать в виде списков и диапазонов.
-        /// Например:
-        ///     1,2,3-5,10-20/3
-        ///     означает список 1,2,3,4,5,10,13,16,19
-        /// Дробью задается шаг в списке.
-        /// Звездочка означает любое возможное значение.
-        /// Например (для часов):
-        ///     */4
-        ///     означает 0,4,8,12,16,20
-        /// Вместо списка чисел месяца можно указать 32. Это означает последнее
-        /// число любого месяца.
-        /// Пример:
-        ///     *.9.*/2 1-5 10:00:00.000
-        ///     означает 10:00 во все дни с пн. по пт. по нечетным числам в сентябре
-        ///     *:00:00
-        ///     означает начало любого часа
-        ///     *.*.01 01:30:00
-        ///     означает 01:30 по первым числам каждого месяца
-        /// </param>
         [Fact]
         public void FullFormat()
         {
@@ -272,6 +239,34 @@ namespace TestApp.UnitTest
                         new[] {ScheduleFormatEntry.SinglePoint(0)}
                     )),
                 ParserHelper.FullFormatParser.ParseOrThrow("10:00:00.000"));
+            
+            AssertEqualFormat(new ScheduleFormat(new ScheduleDate(
+                        new[] {ScheduleFormatEntry.Always},
+                        new[] {ScheduleFormatEntry.Always},
+                        new[] {ScheduleFormatEntry.SinglePoint(1)}
+                    ),
+                    new []{ScheduleFormatEntry.Always},
+                    new ScheduleTime(
+                        new[] {ScheduleFormatEntry.SinglePoint(1)},
+                        new[] {ScheduleFormatEntry.SinglePoint(30)},
+                        new[] {ScheduleFormatEntry.SinglePoint(0)},
+                        new[] {ScheduleFormatEntry.SinglePoint(0)}
+                    )),
+                ParserHelper.FullFormatParser.ParseOrThrow("*.*.01 01:30:00"));
+            
+            AssertEqualFormat(new ScheduleFormat(new ScheduleDate(
+                        new[] {ScheduleFormatEntry.Always},
+                        new[] {ScheduleFormatEntry.Always},
+                        new[] {ScheduleFormatEntry.Always}
+                    ),
+                    new []{ScheduleFormatEntry.Always},
+                    new ScheduleTime(
+                        new[] {ScheduleFormatEntry.Always},
+                        new[] {ScheduleFormatEntry.SinglePoint(0)},
+                        new[] {ScheduleFormatEntry.SinglePoint(0)},
+                        new[] {ScheduleFormatEntry.SinglePoint(0)}
+                    )),
+                ParserHelper.FullFormatParser.ParseOrThrow("*:00:00"));
         }
 
         private static void AssertEqualDates(ScheduleDate expected, ScheduleDate actual)
